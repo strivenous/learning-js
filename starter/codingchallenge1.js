7cd2d116423a6f47439603f3c00ee077996a30ff
strivenous <65793524+strivenous@users.noreply.github.com>
2020-05-26 02:36:57 +0200

    Update codingchallenge1.js

--- starter/codingchallenge1.js
+++ starter/codingchallenge1.js
@@ -25,7 +25,7 @@ theirHeight = prompt("insert their height in meters: ");
 
 var yourBMI = yourMass / (yourHeight**2);
 var theirBMI = theirMass / (theirHeight**2);
-
+// start of spaghetti code
 var youHave_moreBMI = yourBMI > theirBMI;
 var youHave_equalBMI = yourBMI == theirBMI; // equality vs identity
 var equalBMI = (yourBMI*2) / (theirBMI*2);
@@ -35,6 +35,15 @@ var youHave_moreBMImsg, youHave_equalBMImsg, youHave_lessBMImsg;
 youHave_moreBMImsg = yourName + "'s BMI is higher than " + theirName + "'s.\n" + yourBMI + " vs " + theirBMI;
 youHave_equalBMImsg = "both " + yourName + " and " + theirName + " have the same BMI, which is " + equalBMI;
 youHave_lessBMImsg = theirName + "'s BMI is higher than" + yourName + "'s\n" +  theirBMI + " vs " + yourBMI + "\n average BMI is " + averageBMI;
+
+/* end of spaghetti code 
+
+condition variables are not needed, if statements can be simply used as it's implied that the the if (condition) has to always be true
+message variable names have to be shortened
+@TODO optimize this whole thing
+
+*/
+
 
 if (youHave_moreBMI === true) {
 
