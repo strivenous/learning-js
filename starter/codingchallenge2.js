fdb96c191afa3b78619ee9242d45541d9b780022
strivenous <65793524+strivenous@users.noreply.github.com>
2020-05-26 21:32:32 +0200

    Create codingchallenge2.js

---
+++ starter/codingchallenge2.js
@@ -0,0 +1,9 @@
+/** 
+ * JOHN and MIKE both play basketball in different teams. In the latest 3 games,
+ * John's team scored 89, 120 and 103, Mike's scored 116, 94 and 123.
+ * 
+ * 
+ * 
+ * Make an average of teams (also define team names) with infinite amounts of numbers inputtable per each. 
+ * Calculate the average between both and define which wins, then average both teams.
+*/
